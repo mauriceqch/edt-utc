054ef4351b97cccafd0ebec1943743b155db4a77
DSI <dsi@usec-utc.fr>
2016-02-07 13:49:08 +0100

    Clean repo

--- testscript.rb
+++
@@ -1,5 +0,0 @@
-controller = ScriptsController.new
-parsed_script = controller.parse_script(Script.last.script)
-cal = controller.interpret_parsed_script(parsed_script)
-cal.publish
-controller.write_cal(cal)
